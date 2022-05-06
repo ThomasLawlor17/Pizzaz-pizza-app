b5e0ac79c02f3438307bf2b976b005e1a703405c
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2022-05-05 22:29:08 -0400

    FINALLY

--- controllers/randomize.js
+++ controllers/randomize.js
@@ -1,6 +1,7 @@
 const Restaurant = require('../models/restaurant')
 const Pizza = require('../models/pizza')
 const User = require('../models/user')
+const { find } = require('../models/restaurant')
 
 module.exports = {
     index,
@@ -8,18 +9,23 @@ module.exports = {
 
 function index(req, res) {
     if (req.user) {
-    Restaurant.findOne({ $sample: {size: 1 } }, function(err, restaurant) {
-        Pizza.find({ $sample: {size: 1 }}, function(err, pizza) {
+    Restaurant.aggregate([{ $sample: {size: 1 } }], function(err, restaurant) {
+        Pizza.aggregate([{ $sample: {size: 1 }}], function(err, pizza) {
             User.findById(req.user.id, function(err, user) {
+                restaurant = restaurant[0]
+                pizza = pizza[0]
                 console.log(pizza, restaurant)
                 res.render('randomize/index', {title: 'RANDOMIZE', user, pizza, restaurant})
             })
         })
     })
 } else {
-    Restaurant.find({ $sample: {size: 1 } }, function(err, restaurant) {
-        Pizza.find({ $sample: {size: 1 }}, function(err, pizza) {
+    Restaurant.aggregate([{ $sample: {size: 1 } }], function(err, restaurant) {
+        Pizza.aggregate([{ $sample: {size: 1}}], function(err, pizza) {
+            restaurant = restaurant[0]
+            pizza = pizza[0]
+                console.log('REST: ',restaurant._id,'PZZA: ', pizza)
                 res.render('randomize/index', {title: 'RANDOMIZE', user: req.params.user, pizza, restaurant})
+            })
         })
-    })
 }}
